467a12a900079bf15624adc447629f8602fa5755
Rafael Sobral Resende <rafael.resende@edu.pucrs.com>
2022-09-09 08:13:06 -0300

    fifty try

--- src/main/java/br/masmangan/beecrowd/bee1074/Main.java
+++ src/main/java/br/masmangan/beecrowd/bee1074/Main.java
@@ -8,20 +8,23 @@ public class Main {
 
     	Scanner key = new Scanner(System.in);
     	int N = key.nextInt();
+        String eu = "";
 
     	for (int i = 0; i < N; i++) {
     		int x = key.nextInt();
 
-    		if (x == 0) System.out.println("NULL");
+    		if (x == 0) eu = eu + "NULL";
 
-    		    else if (x % 2 == 0 && x > 0) System.out.println("EVEN POSITIVE");
+    		    else if (x % 2 == 0 && x > 0) eu = eu + "EVEN POSITIVE \n";
 
-    		        else if (x % 2 == 0 && x < 0) System.out.println("EVEN NEGATIVE");
+    		        else if (x % 2 == 0 && x < 0) eu = eu + "EVEN NEGATIVE \n";
 
-    		            else if (x % 2 != 0 && x > 0) System.out.println("ODD POSITIVE");
+    		            else if (x % 2 != 0 && x > 0) eu = eu + "ODD POSITIVE \n";
 
-    		                else if (x % 2 != 0 && x < 0) System.out.println("ODD NEGATIVE");
+    		                else if (x % 2 != 0 && x < 0) eu = eu + "ODD NEGATIVE \n";
     	}
+
+        System.out.println(eu);
 	}
 	
 }
